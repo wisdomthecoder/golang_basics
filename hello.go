e985bb442a971c88f1b95b12cf9d471bf5294333
Wisdom Dauda <codewithwise@gmail.com>
2025-08-17 18:39:33 +0100

    Is Perfect Square

--- hello.go
+++ hello.go
@@ -1,21 +1,20 @@
 package main
 
-func main() {
-	for i := 20000000; i <= 10000000000000; i++ {
-		var isPrime bool = true
-		for j := 2; j*j <= i; j++ {
-			if i%j == 0 {
-				isPrime = false
-				break
-			}
-		}
-		if isPrime {
-			println(i)
+import "math"
 
+func main() {
+	for i := 1; i < 1000000; i++ {
+		if isPerfectSquare(float64(i)) {
+			println(i, " is a perfect square")
 		}
 	}
 }
-func add(x, y int) int {
-	return x + y
+func isPerfectSquare(v float64) bool {
+
+	if root := float32(math.Sqrt(v)); root == float32(int(root)) {
+		return true
+	} else {
+		return false
+	}
 
 }
