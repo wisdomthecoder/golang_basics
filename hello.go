b939e5e3768bef10317069d144931aab6d4d597e
Wisdom Dauda <codewithwise@gmail.com>
2025-08-17 19:03:27 +0100

    Structs

--- hello.go
+++ hello.go
@@ -1,19 +1,13 @@
 package main
 
-import (
-	"time"
-)
-
 func main() {
-	t := time.Now()
-	defer println("Hello Wisdom")
+	v := Vertex{Y: 20}
+	v.Y = 20
+	println(v.X)
+	println(&v.Y)
+}
 
-	switch {
-	case t.Hour() < 12:
-		println("Chief Good morning")
-	case t.Hour() < 17:
-		println("Chief Good Afternoon")
-	default:
-		println("Chief Relax")
-	}
+type Vertex struct {
+	X int
+	Y int
 }
