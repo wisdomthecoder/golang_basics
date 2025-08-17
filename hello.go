3abe5b5668ca0d75beb053d4ee8b73da4cc62ca6
Wisdom Dauda <codewithwise@gmail.com>
2025-08-17 18:20:03 +0100

    Prime Numbers Generator

--- hello.go
+++ hello.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
-
 func main() {
-	var i uint16 = 10_000
-	fmt.Println(i)
+	for i := 20000000; i <= 10000000000000; i++ {
+		var isPrime bool = true
+		for j := 2; j*j <= i; j++ {
+			if i%j == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			println(i)
+
+		}
+	}
 }
 func add(x, y int) int {
 	return x + y
