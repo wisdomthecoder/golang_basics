b139db1c9e48c876940550317d88de232d01ff9e
Wisdom Dauda <codewithwise@gmail.com>
2025-08-17 14:38:37 +0100

    alot

--- hello.go
+++ hello.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
+	// "time"
 )
 
-func quadraticSolver(a, b, c float32) (float32, float32) {
-	discriminant := float32(b*b - 4*a*c)
-
-	firstV := (-b + float32(math.Sqrt(float64(discriminant)))) / 2 * a
-	secondV := (-b - float32(math.Sqrt(float64(discriminant)))) / 2 * a
-
-	return firstV, secondV
-}
-
 func main() {
-	a, b := quadraticSolver(1, -5, 4)
-	fmt.Print(a, b)
+	var i uint = 0b01
+	var u int = int(i)
+	fmt.Print(u)
+
 }
